cb3721a3d8ac92567423859f173fff5894bec61b
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-03-27 01:47:36 -0300

    chore: initial project setup

---
+++ easyracha/src/main/java/br/com/thales/easyracha/EasyrachaApplication.java
@@ -0,0 +1,13 @@
+package br.com.thales.easyracha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyrachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyrachaApplication.class, args);
+	}
+
+}
---
+++ easyracha/src/test/java/br/com/thales/easyracha/EasyrachaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thales.easyracha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyrachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
